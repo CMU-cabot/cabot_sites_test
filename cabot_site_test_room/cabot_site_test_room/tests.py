9b49159779a5417999689f9e67d4a73fd9fa326d
Daisuke Sato <daisukes@cmu.edu>
2024-02-21 16:43:30 -0500

    update test room test cases
    Signed-off-by: Daisuke Sato <daisukes@cmu.edu>

--- cabot_site_test_room/cabot_site_test_room/tests.py
+++ cabot_site_test_room/cabot_site_test_room/tests.py
@@ -12,10 +12,19 @@ def wait_ready(tester):
 def test0_navigation_to_a_goal(tester):
     tester.reset_position()
     tester.goto_node('EDITOR_node_1707899365026')
-    tester.wait_navigation_arrived()
+    tester.wait_navigation_arrived(timeout=90)
 
 
-def test1_navigation_to_a_goal(tester):
+# cabot planner can be dead due to a goal that is too close to the current position
+def test1_start_on_narrow_path_start(tester):
     tester.reset_position(x=-0.7, y=0.7, a=90.0)
     tester.goto_node('EDITOR_node_1707899365026')
-    tester.wait_navigation_arrived()
+    tester.wait_navigation_arrived(timeout=90)
+
+
+def test2_navigation_to_a_goal_and_return(tester):
+    tester.reset_position()
+    tester.goto_node('EDITOR_node_1707899365026')
+    tester.wait_navigation_arrived(timeout=90)
+    tester.goto_node('EDITOR_node_1707899162797')
+    tester.wait_navigation_arrived(timeout=90)
