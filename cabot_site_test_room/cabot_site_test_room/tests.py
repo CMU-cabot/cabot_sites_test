a3864df7ab8078f832792862d7ddc003eb22ef63
Daisuke Sato <daisukes@cmu.edu>
2024-03-12 21:50:06 -0400

    test navigation_arrived event timing
    Signed-off-by: Daisuke Sato <daisukes@cmu.edu>

--- cabot_site_test_room/cabot_site_test_room/tests.py
+++ cabot_site_test_room/cabot_site_test_room/tests.py
@@ -10,26 +10,26 @@ def wait_ready(tester):
     tester.wait_localization_started()
 
 
-def _check_interface_event(tester, event, **kwargs):
-    tester.check_topic(**dict(
+def _check_cabot_event(tester, event, **kwargs):
+    return tester.check_topic(**dict(
         dict(
             action_name=f'check_interface_{event}',
-            topic='/cabot/activity_log',
-            topic_type='cabot_msgs/msg/Log',
-            condition=f"msg.category=='cabot/interface' and msg.text=='navigation' and msg.memo=='{event}'",
+            topic='/cabot/event',
+            topic_type='std_msgs/msg/String',
+            condition=f"msg.data=='{event}'",
             timeout=60
         ),
         **kwargs)
     )
 
 
-def _check_interface_event_error(tester, event, **kwargs):
-    tester.check_topic_error(**dict(
+def _check_cabot_event_error(tester, event, **kwargs):
+    return tester.check_topic_error(**dict(
         dict(
             action_name=f'check_interface_{event}_error',
-            topic='/cabot/activity_log',
-            topic_type='cabot_msgs/msg/Log',
-            condition=f"msg.category=='cabot/interface' and msg.text=='navigation' and msg.memo=='{event}'",
+            topic='/cabot/event',
+            topic_type='std_msgs/msg/String',
+            condition=f"msg.data=='{event}'",
             timeout=60
         ),
         **kwargs)
@@ -41,3 +41,14 @@ def test1_navigation_to_a_goal(tester):
     tester.goto_node('EDITOR_node_1707899314416')
     tester.wait_navigation_arrived(timeout=90)
 
+
+# navigation_arrived event should be issued after navigation is completed
+def test2_navigation_to_a_goal_and_check_event(tester):
+    tester.reset_position(x=1.0, y=-1.0, a=0.0)
+    tester.goto_node('EDITOR_node_1707899314416@-90')
+    cancel = _check_cabot_event_error(tester, "navigation_arrived")
+    tester.wait_navigation_arrived()
+    tester.wait_for(seconds=1)
+    cancel()
+    _check_cabot_event(tester, "navigation_arrived")
+    tester.wait_navigation_completed()
