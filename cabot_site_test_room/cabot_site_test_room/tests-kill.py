41bc909cad59f6001f23f0eff19397acef119b34
Daisuke Sato <daisukes@cmu.edu>
2024-08-22 14:23:22 -0400

    add test for dead bt_navigator
    Signed-off-by: Daisuke Sato <daisukes@cmu.edu>

--- cabot_site_test_room/cabot_site_test_room/tests-kill.py
+++ cabot_site_test_room/cabot_site_test_room/tests-kill.py
@@ -53,3 +53,31 @@ def test2_kill_planner(tester):
     tester.wait_for(2)
     tester.goto_node('EDITOR_node_1707899314416')
     tester.wait_navigation_arrived(timeout=30)
+
+def test3_kill_navigator_after_sending_goal(tester):
+    tester.reset_position()
+    tester.info("navigate to a node")
+    tester.goto_node('EDITOR_node_1707899314416')
+    tester.wait_for(3)
+    tester.info("killing bt_navigator with signal 9")
+    pids = _get_ros2_node_pids(tester, "bt_navigator")
+    for pid in pids:
+        try:
+            tester.info(pid)
+            os.kill(pid, 9)
+        except:
+            pass
+    tester.wait_for(2)
+    tester.button_down(3)  # pause
+    tester.wait_for(2)
+    cancel = tester.check_topic(
+        action_name='wait_pausing_message',
+        topic='/cabot/activity_log',
+        topic_type='cabot_msgs/msg/Log',
+        condition="msg.category=='cabot/interface' and msg.text=='navigation' and msg.memo=='pausing'",
+    )
+    tester.button_down(4)  # resume - will timeout to cancel
+    tester.wait_for(7)
+    cancel()
+    tester.button_down(4)  # resume - this may fail due to delay of lifecycle bond check, and will retry to resume
+    tester.wait_navigation_arrived(timeout=30)
