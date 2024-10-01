afc8a1014a98313a0e234ecf5eada89dc6ac5543
Kenta Konagaya <k.konagaya@kufusha.com>
2024-10-01 14:04:55 +0900

    remove experimental code
    Signed-off-by: Kenta Konagaya <k.konagaya@kufusha.com>

--- cabot_site_test_room/cabot_site_test_room/tests-avoid-obstacle.py
+++ cabot_site_test_room/cabot_site_test_room/tests-avoid-obstacle.py
@@ -126,70 +126,13 @@ def test1_climb_step_lower_than_5cm_and_slow_down(tester):
     cancel()
     tester.clean_obstacle()
 
-def test1_recognize_step_higher_than_1cm_and_slow_down(tester):
-    # this does NOT check if the robot slow down yet
-    STEP_HEIGHT = 0.011 # 1.1cm=11mm
-    tester.reset_position(x=0.0, y=0.0, a=0.0)
-    cancel = tester.check_collision_obstacle()
-    tester.spawn_obstacle(
-            name="10mm_step", \
-            x=3, y=0.7, z=0., yaw=0., \
-            width=1, height=1, depth=STEP_HEIGHT \
-            )
-    tester.goto_node('EDITOR_node_1707899150598')
-    tester.wait_navigation_arrived()
-    tester.clean_obstacle()
-    cancel()
-
-#def test2_recognize_step_lower_than_5cm_and_slow_down(tester):
-#    # this does NOT check if the robot slow down yet
-#    STEP_HEIGHT = 0.049 # 4.9cm=49mm
-#    tester.reset_position(x=0.0, y=0.0, a=0.0)
-#    cancel = tester.check_collision_obstacle()
-#    tester.spawn_obstacle(
-#            name="10mm_step", \
-#            x=3, y=0.7, z=0., yaw=0., \
-#            width=1, height=1, depth=STEP_HEIGHT \
-#            )
-#    tester.goto_node('EDITOR_node_1707899150598')
-#    tester.wait_navigation_arrived()
-#    tester.clean_obstacle()
-#    cancel()
-#
-#def test3_recognize_step_higher_than_5cm_and_avoid_step(tester):
-#    STEP_HEIGHT = 0.051 # 5.1cm=51mm
-#    tester.reset_position(x=0.0, y=0.0, a=0.0)
-#    cancel = tester.check_no_collision_obstacle()
-#    tester.spawn_obstacle(
-#            name="10mm_step", \
-#            x=3, y=0.7, z=0., yaw=0., \
-#            width=1, height=1, depth=STEP_HEIGHT \
-#            )
-#    tester.goto_node('EDITOR_node_1707899150598')
-#    tester.wait_navigation_arrived()
-#    tester.clean_obstacle()
-#    cancel()
-
-def test4_recognize_step_higher_than_or_equal_to_20cm_and_avoid_step(tester):
-    STEP_HEIGHT = 1.500 # 20cm=200mm
-    tester.reset_position(x=0.0, y=0.0, a=0.0)
-    cancel = tester.check_no_collision_obstacle()
-    tester.spawn_obstacle(
-            name="10mm_step", \
-            x=3, y=0.7, z=0., yaw=0., \
-            width=1, height=1, depth=STEP_HEIGHT \
-            )
-    tester.goto_node('EDITOR_node_1707899150598')
-    tester.wait_navigation_arrived()
-    tester.clean_obstacle()
-    cancel()
-
-def test999_clean_obstacles(tester):
-    tester.wait_topic(
-        action_name='check_obstacle_states',
-        topic='/obstacle_states',
-        topic_type='pedestrian_plugin_msgs/msg/Agents',
-        condition="True",
-        timeout=10
-    )
-    tester.clean_obstacle()
+# # To execute clean_obstacle, /obstacle_states topic must exist
+# def test999_clean_obstacles(tester):
+#     tester.wait_topic(
+#         action_name='check_obstacle_states',
+#         topic='/obstacle_states',
+#         topic_type='pedestrian_plugin_msgs/msg/Agents',
+#         condition="True",
+#         timeout=10
+#     )
+#     tester.clean_obstacle()
