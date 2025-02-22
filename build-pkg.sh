6bdfb66d103b0e5f1cb83a1948839c4f1d37d177
Daisuke Sato <daisukes@cmu.edu>
2025-02-23 04:31:41 +0900

    release v1.0.1
    Signed-off-by: Daisuke Sato <daisukes@cmu.edu>

--- build-pkg.sh
+++ build-pkg.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
+# set CABOT_SITE to the current dir name
 CABOT_SITE=$1
+VERSION=$2
 
-if [[ -z $CABOT_SITE ]]; then
+if [[ -z $VERSION ]]; then
     echo "Usage:"
-    echo " $0 <cabot_site pkg name>"
+    echo " $0 <cabot_site pkg version>"
     exit
 fi
 
-echo "build $CABOT_SITE package"
+echo "build $CABOT_SITE package version $VERSION"
+
+# check if ${VERSION} (${VERSION#v}) is matched with version in $CABOT_SITE/package.xml
+if ! grep -q "<version>${VERSION#v}</version>" ./${CABOT_SITE}/package.xml; then
+    echo "Version ${VERSION#v} is not matched with version in ${CABOT_SITE}/package.xml: $(grep '<version>' ./${CABOT_SITE}/package.xml)"
+    # make a tmpfile
+    tmpfile=$(mktemp)
+    sed "s|<version>.*</version>|<version>${VERSION#v}</version>|" ./${CABOT_SITE}/package.xml > $tmpfile
+    mv $tmpfile ./${CABOT_SITE}/package.xml
+    echo "Version updated to $(grep '<version>' ./${CABOT_SITE}/package.xml)"
+    exit 1
+fi
 
 docker run --rm \
-    -v ./pkg:/opt/build_ws/pkg \
+    -v ./pkg:/opt/build_ws/install \
     -v ./${CABOT_SITE}:/opt/build_ws/src/${CABOT_SITE} \
     ros:humble \
     /bin/sh -c "\
-        apt update && \
-        apt install -y zip && \
-	. /opt/ros/humble/setup.sh && \
-	cd /opt/build_ws && colcon build && \
-	cd install && \
-	zip -qr ../pkg/${CABOT_SITE}.zip ${CABOT_SITE} && \
-        chown $UID:$UID ../pkg/${CABOT_SITE}.zip \
-	"
+        . /opt/ros/humble/setup.sh && \
+        cd /opt/build_ws && colcon build \
+    "
+
+zip -qr ./pkg/${CABOT_SITE}-${VERSION}.zip ./pkg/${CABOT_SITE}
+
+# delete all files and dirs in pkg dir except the zip file and .gitignore (this is for test build)
+find ./pkg -mindepth 1 -maxdepth 1 -type d -exec rm -r {} \;
+find ./pkg -mindepth 1 -maxdepth 1 -type f ! -name "*-${VERSION}.zip" ! -name .gitignore -exec rm {} \;
