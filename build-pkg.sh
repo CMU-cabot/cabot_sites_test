cccee75bf89eef59893b08943b4f9d8288b1fb60
Daisuke Sato <daisukes@cmu.edu>
2025-05-19 21:07:50 -0400

    update build-pkg.sh
    Signed-off-by: Daisuke Sato <daisukes@cmu.edu>

--- build-pkg.sh
+++ build-pkg.sh
@@ -1,43 +1,28 @@
 #!/bin/bash
 
-# set CABOT_SITE to the current dir name
 CABOT_SITE=$1
-VERSION=$2
 
-if [[ -z $VERSION ]]; then
+if [[ -z $CABOT_SITE ]]; then
     echo "Usage:"
-    echo " $0 <cabot_site pkg version>"
+    echo " $0 <cabot_site> <pkg name>"
     exit
 fi
 
-echo "build $CABOT_SITE package version $VERSION"
-
-# echo $VERSION without prefix v and postfix -something
-SHORT_VERSION=$(echo $VERSION | sed -e 's/^v//' -e 's/-.*//')
-
-# check if ${VERSION} (${VERSION#v}) is matched with version in $CABOT_SITE/package.xml
-if ! grep -q "<version>${SHORT_VERSION}</version>" ./${CABOT_SITE}/package.xml; then
-    echo "Version ${SHORT_VERSION} is not matched with version in ${CABOT_SITE}/package.xml: $(grep '<version>' ./${CABOT_SITE}/package.xml)"
-    # make a tmpfile
-    tmpfile=$(mktemp)
-    sed "s|<version>.*</version>|<version>${SHORT_VERSION}</version>|" ./${CABOT_SITE}/package.xml > $tmpfile
-    mv $tmpfile ./${CABOT_SITE}/package.xml
-    echo "Version updated to $(grep '<version>' ./${CABOT_SITE}/package.xml)"
-    exit 1
-fi
+echo "build $CABOT_SITE package"
 
 docker run --rm \
-    -v ./pkg:/opt/build_ws/install \
+    -v ./pkg:/opt/build_ws/pkg \
     -v ./${CABOT_SITE}:/opt/build_ws/src/${CABOT_SITE} \
     ros:humble \
     /bin/sh -c "\
+        apt update && \
+        apt install -y zip && \
         . /opt/ros/humble/setup.sh && \
-        cd /opt/build_ws && colcon build \
-    "
-
-pushd pkg
-zip -qr ./${CABOT_SITE}-${VERSION}.zip ./${CABOT_SITE}
-
-# delete all files and dirs in pkg dir except the zip file and .gitignore (this is for test build)
-find ./ -mindepth 1 -maxdepth 1 -type d -exec rm -r {} \;
-find ./ -mindepth 1 -maxdepth 1 -type f ! -name "*-${VERSION}.zip" ! -name .gitignore -exec rm {} \;
+        cd /opt/build_ws && colcon build && \
+        cd install && \
+        zip -qr ../pkg/${CABOT_SITE}.zip ${CABOT_SITE} && \
+        chown $UID:$UID ../pkg/${CABOT_SITE}.zip && \
+        cd /opt/build_ws/src/${CABOT_SITE} && \
+        zip -qr ../../pkg/${CABOT_SITE}-server_data.zip server_data && \
+        chown $UID:$UID ../../pkg/${CABOT_SITE}-server_data.zip \
+	"
